218ccd2b7466206c649c72b87d3c757243b1573e
maclemama <maclema@me.com>
2023-08-22 14:58:04 +0100

    Create and display home page empty component and install basic packages for routing, scss and api call

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className="placeholder">Pathfinder Map</h1>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<div className="App">
+				<h1 className="placeholder">Pathfinder Map</h1>
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+        </Routes>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,9 @@
+import "./HomePage.scss";
+
+function HomePage(){
+    return(
+        <h1 className="page">this is home page</h1>
+    )
+}
+
+export default HomePage;
