b89d5a0e823b8da7dfd4e393759d497f64d76180
maclemama <maclema@me.com>
2023-08-26 11:55:06 +0100

    Add comment of the reason regarding turn off strict mode

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
-    <App />
-  // {/* </React.StrictMode> */}
+	// <React.StrictMode> // turn off strict mode as marker and any route on the map cannot be displayed if it is on
+	<App />
+	// </React.StrictMode>
 );
